c4a0466b76d9d1e60db03b27bcff23d710f865fa
davm07 <diegovasquezmejia1999@gmail.com>
2024-03-02 19:04:54 +0100

    Finish first class of the course

--- 3.fs-readFile.js
+++
@@ -1,5 +0,0 @@
-const fs = require("node:fs");
-
-const text = fs.readFileSync('./archivo.txt', 'utf-8');
-
-console.log(text)
--- clase1/1.os-info.mjs
+++ clase1/1.os-info.mjs
@@ -1,4 +1,12 @@
-import { platform, release, arch, cpus, freemem, totalmem, uptime } from 'node:os'
+import {
+  platform,
+  release,
+  arch,
+  cpus,
+  freemem,
+  totalmem,
+  uptime
+} from 'node:os'
 
 console.log('Informacion del sistema operativo:')
 console.log('---------------------')
@@ -11,8 +19,8 @@ console.log('Arquitectura', arch())
 
 console.log('CPUs', cpus())
 
-console.log("Memoria Libre", freemem() / 1024 / 1024);
+console.log('Memoria Libre', freemem() / 1024 / 1024)
 
 console.log('Memoria total', totalmem() / 1024 / 1024)
 
-console.log('Uptime', uptime() / 60 /60)
+console.log('Uptime', uptime() / 60 / 60)
